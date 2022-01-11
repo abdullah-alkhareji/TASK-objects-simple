8ac861fdb2059a1d7195ecf01ee1b7e7a5eeeaad
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-11 18:40:51 +0300

    done

--- index.js
+++ index.js
@@ -12,6 +12,31 @@
  */
 
 // add the movies variables here ....
+const movie1 = {
+	id: 1,
+	name: "Spider-man",
+	image:
+		"https://www.cnet.com/a/img/-E8L4Ahc9CSYCXJGL_i5hU2ezIs=/0x0:850x478/1200x675/2021/11/29/82fb5acc-9155-4844-be9c-e0831a6b837c/nowayhome.jpg",
+	rating: 10,
+};
+const movie2 = {
+	id: 2,
+	name: "Hulk",
+	image:
+		"https://www.themoviedb.org/t/p/original/gKzYx79y0AQTL4UAk1cBQJ3nvrm.jpg",
+	rating: 6.7,
+};
+const movie3 = {
+	id: 3,
+	name: "Man of Steel",
+	image:
+		"https://i.pinimg.com/originals/61/b1/22/61b122e81b1946392ffc903cd098e439.jpg",
+	rating: 8.9,
+};
+
+console.log(movie1);
+console.log(movie2);
+console.log(movie3);
 
 /** Task 2:
  * Create an array `movies`
@@ -29,6 +54,31 @@
  *    },
  * ]
  */
+
+// const movies = [movie1, movie2, movie3];
+const movies = [
+	{
+		id: 1,
+		name: "Spider-man",
+		image:
+			"https://www.cnet.com/a/img/-E8L4Ahc9CSYCXJGL_i5hU2ezIs=/0x0:850x478/1200x675/2021/11/29/82fb5acc-9155-4844-be9c-e0831a6b837c/nowayhome.jpg",
+		rating: 10,
+	},
+	{
+		id: 2,
+		name: "Hulk",
+		image:
+			"https://www.themoviedb.org/t/p/original/gKzYx79y0AQTL4UAk1cBQJ3nvrm.jpg",
+		rating: 6.7,
+	},
+	{
+		id: 3,
+		name: "Man of Steel",
+		image:
+			"https://i.pinimg.com/originals/61/b1/22/61b122e81b1946392ffc903cd098e439.jpg",
+		rating: 8.9,
+	},
+];
 
 /** TASK 3:
  * Create a new file call it `movies.json`
